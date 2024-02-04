58cc01dd17c260b880474f70a016b4e2b21c383a
CrepMC <106760948+CrepMC@users.noreply.github.com>
2024-02-04 04:32:57 +0000

    16

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ document.addEventListener('contextmenu', function (e) {
 
 function handleMouseOver(caretId, itemClass) {
   document.getElementById(caretId).addEventListener('mouseover', function () {
-    document.querySelector('.' + itemClass).style.display = 'block';
+    document.querySelector('.' + itemClass).style.display = 'flex';
   });
 
   document.getElementById(caretId).addEventListener('mouseout', function () {
@@ -25,5 +25,7 @@ function handleMouseOver(caretId, itemClass) {
   });
 }
 
+
 handleMouseOver('caret-down-1', 'tet-gift-item');
 handleMouseOver('caret-down-2', 'tet-gift-item');
+handleMouseOver('tet-gift-item', 'tet-gift-item');
